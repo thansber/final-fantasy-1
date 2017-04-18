130738bf3be62ba9f31050b95a2003dbdff47191
thansber <thansber@yahoo.com>
2017-04-18 06:55:13 -0400

    add grunt task to concat JSON for maps

---
+++ Gruntfile.js
@@ -0,0 +1,25 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    'concat-json': {
+      maps: {
+        base: 'src/ff-maps/map-data',
+        src: 'src/ff-maps/map-data/**/*.json',
+        dest: 'src/ff-maps/map-data.json',
+        //transforms: ['towns'],
+        options: {
+          space: '  '
+        }
+      }
+    },
+    watch: {
+      files: ['']
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-concat-json');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('default');
+
+};
