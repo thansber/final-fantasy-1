956d60b240500947eb814f50724e1e2c797ad054
thansber <thansber@yahoo.com>
2017-04-27 16:12:12 -0400

    move char classes to separate behavior

--- src/ff-char-select/ff-char-selector.js
+++ src/ff-char-select/ff-char-selector.js
@@ -1,58 +1,65 @@
-Polymer({
-  is: 'ff-char-selector',
-  properties: {
-    charClassData: {
-      type: Array,
-      value: function() {
-        return [
-          { charClass: 'fighter', label: 'FIGHTER' },
-          { charClass: 'thief', label: 'THIEF' },
-          { charClass: 'blackbelt', label: 'Bl.BELT' },
-          { charClass: 'redmage', label: 'RedMAGE' },
-          { charClass: 'whitemage', label: 'Wh.MAGE' },
-          { charClass: 'blackmage', label: 'Bl.MAGE' }
-        ]
+(function(scope) {
+  scope.FF = scope.FF || {};
+
+  Polymer({
+    is: 'ff-char-selector',
+    behaviors: [
+      scope.FF.CharClassBehavior
+    ],
+    properties: {
+      charClassData: {
+        type: Array,
+        value: function() {
+          return [
+            { charClass: 'fighter', label: 'FIGHTER' },
+            { charClass: 'thief', label: 'THIEF' },
+            { charClass: 'blackbelt', label: 'Bl.BELT' },
+            { charClass: 'redmage', label: 'RedMAGE' },
+            { charClass: 'whitemage', label: 'Wh.MAGE' },
+            { charClass: 'blackmage', label: 'Bl.MAGE' }
+          ]
+        }
+      },
+      charName: {
+        type: String
+      },
+      label: {
+        type: String,
+        computed: '_labelFor(selected)'
+      },
+      selected: {
+        notify: true,
+        type: String,
+        value: 'fighter'
       }
     },
-    charName: {
-      type: String
+
+    next: function() {
+      var index = this._indexOf(this.selected) + 1;
+      if (index === this.basicClasses.length) {
+        index = 0;
+      }
+      this.selected = this.basicClasses[index].charClass;
     },
-    label: {
-      type: String,
-      computed: '_labelFor(selected)'
+
+    prev: function() {
+      var index = this._indexOf(this.selected) - 1;
+      if (index < 0) {
+        index = this.basicClasses.length - 1;
+      }
+      this.selected = this.basicClasses[index].charClass;
     },
-    selected: {
-      notify: true,
-      type: String,
-      value: 'fighter'
+
+    _byCharClass: function(selectedCharClass, data) {
+      return data.charClass === selectedCharClass;
+    },
+
+    _indexOf: function(charClass) {
+      return this.basicClasses.findIndex(this._byCharClass.bind(this, charClass));
+    },
+
+    _labelFor: function(selectedCharClass) {
+      return (this.basicClasses.find(this._byCharClass.bind(this, selectedCharClass)) || {}).label;
     }
-  },
-
-  next: function() {
-    var index = this._indexOf(this.selected) + 1;
-    if (index === this.charClassData.length) {
-      index = 0;
-    }
-    this.selected = this.charClassData[index].charClass;
-  },
-
-  prev: function() {
-    var index = this._indexOf(this.selected) - 1;
-    if (index < 0) {
-      index = this.charClassData.length - 1;
-    }
-    this.selected = this.charClassData[index].charClass;
-  },
-
-  _byCharClass: function(selectedCharClass, data) {
-    return data.charClass === selectedCharClass;
-  },
-
-  _indexOf: function(charClass) {
-    return this.charClassData.findIndex(this._byCharClass.bind(this, charClass));
-  },
-
-  _labelFor: function(selectedCharClass) {
-    return (this.charClassData.find(this._byCharClass.bind(this, selectedCharClass)) || {}).label;
-  }
-});
+  });
+}(window));
