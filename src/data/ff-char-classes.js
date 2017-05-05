7a1a60299e0e87bdbe5d2c07a0d9f429c96e856d
thansber <thansber@yahoo.com>
2017-05-05 07:00:01 -0400

    add advanced char classes

--- src/data/ff-char-classes.js
+++ src/data/ff-char-classes.js
@@ -14,19 +14,66 @@
   var whitemage = 'whitemage';
   var blackmage = 'blackmage';
 
+  var knight = 'knight';
+  var ninja = 'ninja';
+  var master = 'master';
+  var redwizard = 'redwizard';
+  var whitewizard = 'whitewizard';
+  var blackwizard = 'blackwizard';
+
   function CharClass(id, options) {
     this.id = id;
     this.label = options.label;
+    this.walkRow = options.walkRow;
 
     allClassesById[id] = this;
   }
 
-  new CharClass(fighter, { label: 'FIGHTER' });
-  new CharClass(thief, { label: 'THIEF' });
-  new CharClass(blackbelt, { label: 'Bl.BELT' });
-  new CharClass(redmage, { label: 'RedMAGE' });
-  new CharClass(whitemage, { label: 'Wh.MAGE' });
-  new CharClass(blackmage, { label: 'Bl.MAGE' });
+  // basic classes
+  new CharClass(fighter, {
+    label: 'FIGHTER',
+    walkRow: 0
+  });
+  new CharClass(thief, {
+    label: 'THIEF',
+    walkRow: 1
+  });
+  new CharClass(blackbelt, {
+    label: 'Bl.BELT',
+    walkRow: 2
+  });
+  new CharClass(redmage, {
+    label: 'RedMAGE',
+    walkRow: 3
+  });
+  new CharClass(whitemage, {
+    label: 'Wh.MAGE',
+    walkRow: 4
+  });
+  new CharClass(blackmage, {
+    label: 'Bl.MAGE',
+    walkRow: 5
+  });
+
+  // advanced classes
+  new CharClass(knight, {
+    walkRow: 6
+  });
+  new CharClass(ninja, {
+    walkRow: 7
+  });
+  new CharClass(master, {
+    walkRow: 8
+  });
+  new CharClass(redwizard, {
+    walkRow: 9
+  });
+  new CharClass(whitewizard, {
+    walkRow: 10
+  });
+  new CharClass(blackwizard, {
+    walkRow: 11
+  });
 
   var basicClassIndex = function(id) {
     return basicClasses.findIndex(function(charClass) {
