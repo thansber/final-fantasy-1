e3c6b5afc477cef0485f81c124fa7378d38be5c1
thansber <thansber@yahoo.com>
2017-05-05 07:01:10 -0400

    move char walking logic to its own tag

---
+++ src/ff-char-walking/ff-char-walking.js
@@ -0,0 +1,88 @@
+(function(scope) {
+  scope.FF = scope.FF || {};
+
+  Polymer({
+    is: 'ff-char-walking',
+
+    behaviors: [
+      scope.FF.CanvasBehavior
+    ],
+
+    properties: {
+      charClass: {
+        type: String
+      },
+      direction: {
+        type: String,
+        value: 'down'
+      },
+      sheetIconSize: {
+        readonly: true,
+        type: Number,
+        value: 16
+      },
+      sheetLoaded: Boolean,
+      walkCol: Number,
+      walkColsByDirection: {
+        type: Object,
+        value: function() {
+          return {
+            down: 0,
+            left: 2,
+            up: 4,
+            right: 6
+          }
+        }
+      },
+      walkRow: Number
+    },
+
+    observers: [
+      '_onSheetLoaded(sheetLoaded)',
+      '_somethingChanged(charClass, direction)'
+    ],
+
+    ready: function() {
+      this.canvas = this.$.canvas;
+      this.ctx = this.canvas.getContext('2d');
+
+      this._resizeCanvas(this.canvas, 1, 1);
+      this._loadSheet();
+    },
+
+    _drawChar: function(isStepping) {
+      this.ctx.drawImage(
+        this.sheet,
+        this.sheetIconSize * (isStepping ? this.walkCol + 1 : this.walkCol), // y in source
+        this.sheetIconSize * this.walkRow, // x in source
+        this.sheetIconSize, // x-scale on source
+        this.sheetIconSize, // y-scale on source
+        0,
+        0,
+        this.scale,
+        this.scale);
+    },
+
+    _loadSheet: function() {
+      this.sheet = new Image();
+      this.sheet.addEventListener('load', function() {
+        this.set('sheetLoaded', true);
+      }.bind(this), false);
+      this.sheet.src = this.resolveUrl('char-walking.png');
+    },
+
+    _onSheetLoaded: function(sheetLoaded) {
+      if (!sheetLoaded) {
+        return;
+      }
+
+      this._drawChar();
+    },
+
+    _somethingChanged: function(charClass, direction) {
+      this.set('walkRow', scope.FF.CharClasses.fromId(charClass).walkRow);
+      this.set('walkCol', this.walkColsByDirection[direction]);
+      this._onSheetLoaded(this.sheetLoaded);
+    }
+  });
+}(window));
