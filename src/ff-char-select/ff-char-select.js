5cdc998e0a4fad5c33730cad6d964a79e4ffc4b2
thansber <thansber@yahoo.com>
2017-08-02 16:23:42 -0400

    move char selector demo to proper dir

--- src/ff-char-select/ff-char-select.js
+++ src/ff-char-select/ff-char-select.js
@@ -1,52 +1,46 @@
-((scope) => {
 
-  scope.FF = scope.FF || {};
+// TODO: screen mixin
+class CharSelectElement extends CharClassMixin(Polymer.Element) {
+  static get is() { return 'ff-char-select'; }
 
-  const CharClassMixin = scope.FF.CharClassMixin;
-  // TODO: screen mixin
-
-  class CharSelectElement extends CharClassMixin(Polymer.Element) {
-    static get is() { return 'ff-char-select'; }
-
-    static get properties() {
-      return {
-        charIndex: {
-          readonly: true,
-          type: Number
-        },
-        party: {
-          notify: true,
-          type: Array
-        }
-      };
-    }
-
-    _charClassFor(change, index) {
-      return this._charProperty(change, index, 'charClass') || this.CharClass.Fighter;
-    },
-
-    _charProperty(change, index, property) {
-      return this.get(property, change.base[index]);
-    },
-
-    _next() {
-      this.fire('ff-char-class-done', {
-        charClass: this._selectedCharSelector().selected
-      });
-    },
-
-    _nextCharClass() {
-      this._selectedCharSelector().next();
-    },
-
-    _prevCharClass() {
-      this._selectedCharSelector().prev();
-    },
-
-    _selectedCharSelector() {
-      return this.$.selector.selectedItem;
-    }
+  static get properties() {
+    return {
+      charIndex: {
+        readonly: true,
+        type: Number
+      },
+      party: {
+        notify: true,
+        type: Array
+      }
+    };
   }
 
-  customElements.define(CharSelectElement.is, CharSelectElement);
-}(window));
+  _charClassFor(change, index) {
+    return this._charProperty(change, index, 'charClass') || this.CharClass.Fighter;
+  },
+
+  _charProperty(change, index, property) {
+    return this.get(property, change.base[index]);
+  },
+
+  _next() {
+    this.fire('ff-char-class-done', {
+      charClass: this._selectedCharSelector().selected
+    });
+  },
+
+  _nextCharClass() {
+    this._selectedCharSelector().next();
+  },
+
+  _prevCharClass() {
+    this._selectedCharSelector().prev();
+  },
+
+  _selectedCharSelector() {
+    return this.$.selector.selectedItem;
+  }
+}
+
+customElements.define(CharSelectElement.is, CharSelectElement);
