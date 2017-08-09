1d79c26af85c62a26241597e5923fffa394915d8
thansber <thansber@yahoo.com>
2017-08-09 06:54:47 -0400

    get char select screen working, hooked up to Redux

--- src/ff-char-select/ff-char-select.js
+++ src/ff-char-select/ff-char-select.js
@@ -1,6 +1,6 @@
 
 // TODO: screen mixin
-class CharSelectElement extends CharClassMixin(Polymer.Element) {
+class CharSelectElement extends ScreenMixin(CharClassMixin(ReduxMixin(Polymer.Element))) {
   static get is() { return 'ff-char-select'; }
 
   static get properties() {
@@ -10,7 +10,7 @@ class CharSelectElement extends CharClassMixin(Polymer.Element) {
         type: Number
       },
       party: {
-        notify: true,
+        statePath: 'party',
         type: Array
       }
     };
@@ -18,25 +18,27 @@ class CharSelectElement extends CharClassMixin(Polymer.Element) {
 
   _charClassFor(change, index) {
     return this._charProperty(change, index, 'charClass') || this.CharClass.Fighter;
-  },
+  }
 
   _charProperty(change, index, property) {
     return this.get(property, change.base[index]);
-  },
+  }
 
   _next() {
-    this.fire('ff-char-class-done', {
-      charClass: this._selectedCharSelector().selected
+    this.dispatch({
+      type: 'CHAR_CLASS_SET',
+      charClass: this._selectedCharSelector().selected,
+      charIndex: this.charIndex
     });
-  },
+  }
 
   _nextCharClass() {
     this._selectedCharSelector().next();
-  },
+  }
 
   _prevCharClass() {
     this._selectedCharSelector().prev();
-  },
+  }
 
   _selectedCharSelector() {
     return this.$.selector.selectedItem;
