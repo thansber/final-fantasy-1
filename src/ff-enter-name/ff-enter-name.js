853d4f4a130050a878259443ddf52867495e23fc
thansber <thansber@yahoo.com>
2017-08-09 16:25:47 -0400

    enter name screen works

--- src/ff-enter-name/ff-enter-name.js
+++ src/ff-enter-name/ff-enter-name.js
@@ -1,19 +1,30 @@
-class EnterNameElement extends ScreenMixin(Polymer.Element) {
+class EnterNameElement extends ScreenMixin(ReduxMixin(Polymer.Element)) {
   static get is() { return 'ff-enter-name'; }
 
   static get properties() {
     return {
       itemsPerRow: {
+        readonly: true,
         type: Number,
         value: 10
       },
-      maxChars: {
+      maxNameSize: {
+        readonly: true,
+        type: Number,
+        value: 4
+      },
+      maxPartySize: {
+        readonly: true,
         type: Number,
         value: 4
       },
       name: {
         type: String,
         value: ''
+      },
+      party: {
+        statePath: 'party',
+        type: Array
       }
     };
   }
@@ -25,16 +36,23 @@ class EnterNameElement extends ScreenMixin(Polymer.Element) {
     this.name = this.name.substring(0, this.name.length - 1);
   }
 
+  isPartyFull() {
+    return this.party.length >= this.maxPartySize;
+  }
+
   letterSelected(e, detail) {
-    if (this.name.length >= this.maxChars) {
-      this.dispatchEvent(new CustomEvent('ff-char-name-done', {
-        bubbles: true,
-        composed: true,
-        detail: {
-          name: this.name
-        }
-      }));
+    if (this.name.length >= this.maxNameSize) {
+      this.dispatch({
+        type: 'CHAR_NAME_SET',
+        name: this.name
+      });
       this.name = '';
+
+      if (this.isPartyFull()) {
+        this.dispatch({ type: 'START_GAME' });
+      } else {
+        this.dispatch('screenChanged', 'charSelect');
+      }
       return;
     }
     this.name += detail.value;
